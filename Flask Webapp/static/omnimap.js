7b92293949a3199f47b232b0096691bddd478586
Ryan Hattie <hexaguin@gmail.com>
2019-03-03 12:14:21 -0700

    Made zoom increments finer for a smoother mobile experience.

--- Flask Webapp/static/omnimap.js
+++ Flask Webapp/static/omnimap.js
@@ -1,4 +1,6 @@
-var omnimap = L.map('map-container').setView([51.0486, -114.0708], 11);
+var omnimap = L.map('map-container', {
+	zoomSnap: 0.25
+}).setView([51.0486, -114.0708], 11);
 
 // Basemaps
 
