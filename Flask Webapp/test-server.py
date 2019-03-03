4d3a280429649102eaf910b8ae0dc11e71b4bd01
Ryan Hattie <hexaguin@gmail.com>
2019-03-03 14:29:06 -0700

    Unneeded semicolon removed

--- Flask Webapp/test-server.py
+++ Flask Webapp/test-server.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/omnimap/api/cameras')
 def cameras():
-    return omnimap.get_camera_geojson();
+    return omnimap.get_camera_geojson()
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8083, use_reloader=True, use_debugger=True)
